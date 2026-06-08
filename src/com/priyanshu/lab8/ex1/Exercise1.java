617874066d0ec5fc1b4076dbc862f7feac525c6f
Priyanshu Rastogi <83832376+Oriyans-sunset@users.noreply.github.com>
2026-06-08 16:53:32 +0530

    Added ex 1

---
+++ src/com/priyanshu/lab8/ex1/Exercise1.java
@@ -0,0 +1,10 @@
+package com.priyanshu.lab8.ex1;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+    }
+}
