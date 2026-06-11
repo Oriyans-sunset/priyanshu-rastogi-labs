c2c99828f7eb685d2200a42cf71dc15c72fa958c
Priyanshu Rastogi <83832376+Oriyans-sunset@users.noreply.github.com>
2026-06-11 16:48:29 +0530

    Added spring demo

--- codes/com-ibm-spring-demo/src/main/java/com/ibm/demo/App.java
+++ codes/com-ibm-spring-demo/src/main/java/com/ibm/demo/App.java
@@ -1,5 +1,11 @@
 package com.ibm.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.ibm.demo.model.Employee;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello world!" );
+        
     }
 }
